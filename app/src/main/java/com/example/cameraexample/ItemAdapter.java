cb64d5e06fe7d90f54b286336d72bc70cb520675
SEUNGJAE-97 <21813633@yu.ac.kr>
2023-05-25 22:21:25 +0900

    Done :   RecyclerView 구현    - 리스트 delete 기능    - RecyclerView의 onClickListener 구현
    해야할 것:
      RecyclerView 구현
       - 리스트 delete시 DB상에서도 지워지지도록 php 코드 작성
       - 리스트의 TextView에 사용자 DB의 Medicine_ID를 통해 Medicine_Name을 출력하도록
         php코드 작성.
       - 리스트 클릭시 activity_result 레이아웃 불러와서 알약 DB 출력하기
    ***앱 최소 기능 ***

---
+++ app/src/main/java/com/example/cameraexample/ItemAdapter.java
@@ -0,0 +1,103 @@
+package com.example.cameraexample;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
+    ArrayList<Item> items = new ArrayList<Item>();
+    Context context;
+    int lastPosition = -1;
+    private OnItemClickListener listener;
+
+    //수정...중..
+    public interface OnItemClickListener{
+        void onItemClick(Item item);
+    }
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.activity_item, viewGroup,false);
+        context =viewGroup.getContext();
+
+        return new ViewHolder(itemView, listener, items);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+
+        if (viewHolder.getAdapterPosition() > lastPosition){
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.slide_in_row);
+            ((ViewHolder)viewHolder).itemView.startAnimation(animation);
+            /*
+            Item item = items.get(position);
+            viewHolder.setItem(item);
+
+             */
+        }
+        Item item = items.get(position);
+        viewHolder.setItem(item);
+    }
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+    @Override
+    public int getItemCount(){
+        return items.size();
+    }
+    public void addItem(int position, Item item){
+        items.add(position, item);
+    }
+    public void removeItem(int position){
+        items.remove(position);
+    }
+    public void removeAllItem(){
+        items.clear();
+    }
+    public Item getItem(int position){
+        return items.get(position);
+    }
+
+
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title_view;
+        TextView description;
+        public ViewHolder(@NonNull View itemView, final OnItemClickListener listener,final ArrayList<Item> items) {
+            super(itemView);
+            title_view = itemView.findViewById(R.id.title_text);
+            description = itemView.findViewById(R.id.desc_text);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+                    if(position != RecyclerView.NO_POSITION && listener != null){
+                        listener.onItemClick(items.get(position));
+                    }
+                }
+            });
+        }
+        public void setItem(Item item){
+            title_view.setText(item.getTitle());
+            description.setText(item.getDescription());
+        }
+    }
+}
--- app/src/main/java/com/example/cameraexample/ListActivity.java
+++ app/src/main/java/com/example/cameraexample/ListActivity.java
@@ -1,12 +1,95 @@
 package com.example.cameraexample;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class ListActivity extends AppCompatActivity{
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+
+public class ListActivity extends AppCompatActivity implements ItemAdapter.OnItemClickListener{
+    RecyclerView recyclerView;
+    ItemAdapter itemAdapter;
+    ArrayList<Item>items = new ArrayList<Item>();
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView( R.layout.activity_list);
+        recyclerView = findViewById(R.id.recycle_view);
+
+
+        itemAdapter = new ItemAdapter();
+        recyclerView.setAdapter(itemAdapter);
+        itemAdapter.setOnItemClickListener(this);
+
+
+
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT){
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                switch(direction){
+                    case ItemTouchHelper.LEFT:
+                        Item deleteItem = items.get(position);
+
+                        items.remove(position);
+                        itemAdapter.removeItem(position);
+                        itemAdapter.notifyItemRemoved(position);
+                        break;
+                }
+            }
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY,
+                                    int actionState, boolean isCurrentlyActive){
+                new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder,
+                        dX, dY, actionState, isCurrentlyActive)
+                        .addSwipeLeftBackgroundColor(Color.RED)
+                        .addSwipeLeftLabel("remove")
+                        .addSwipeLeftActionIcon(R.mipmap.ic_delete)
+                        .setSwipeLeftLabelColor(Color.WHITE)
+                        .create()
+                        .decorate();
+                super.onChildDraw(c, recyclerView,viewHolder,dX,dY,actionState, isCurrentlyActive);
+            }
+        }).attachToRecyclerView(recyclerView);
+
+        itemAdapter.removeAllItem();
+        for (int i =0; i<10; i++){
+            Item item = new Item();
+            item.setTitle("title"+i);
+            item.setDescription("description"+i);
+
+            items.add(item);
+            itemAdapter.addItem(item);
+        }
+        itemAdapter.notifyDataSetChanged();
+        recyclerView.startLayoutAnimation();
+    }
+    @Override
+    public void onItemClick(Item item){
+        Toast.makeText(this, "clicked : "+item.getTitle(), Toast.LENGTH_SHORT).show();
+
     }
 }
